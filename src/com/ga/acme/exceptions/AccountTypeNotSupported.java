03c4355ac5cbdb9715c29130554c070ebaad2271
MariamBaloch <maryamhoth@gmail.com>
2026-09-10 11:11:58 +0300

    create account type not supported exception

--- src/com/ga/acme/exceptions/AccountTypeNotSupported.java
+++ src/com/ga/acme/exceptions/AccountTypeNotSupported.java
@@ -2,6 +2,6 @@ package com.ga.acme.exceptions;
 
 public class AccountTypeNotSupported extends Exception {
     public AccountTypeNotSupported() {
-        super("Account Type Not Supported, Enter either savings or checking");
+        super("Account Type Not Supported, Enter either savingsaccount or checkingaccount");
     }
 }
