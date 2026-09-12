15f4394f48d871016d0e31cabe7e45413f798172
MariamBaloch <maryamhoth@gmail.com>
2026-09-12 10:26:56 +0300

    create DailyLimitExceededException

---
+++ src/com/ga/acme/exceptions/DailyLimitExceededException.java
@@ -0,0 +1,7 @@
+package com.ga.acme.exceptions;
+
+public class DailyLimitExceededException extends Exception {
+    public DailyLimitExceededException(String message) {
+        super(message);
+    }
+}
