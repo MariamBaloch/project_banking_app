f4cddcd49f83bb02296e9ac8aaf3402b0f376236
MariamBaloch <maryamhoth@gmail.com>
2026-09-14 12:21:20 +0300

    create accountstatement and datefilter functions

---
+++ src/com/ga/acme/enums/DateFilters.java
@@ -0,0 +1,20 @@
+package com.ga.acme.enums;
+
+public enum DateFilters {
+    TODAY("Today"),
+    YESTERDAY("Yesterday"),
+    LAST_WEEK("Last Week"),
+    LAST_7_DAYS("Last 7 Days"),
+    LAST_30_DAYS("Last 30 Days"),
+    LAST_MONTH("Last Month");
+
+    private final String displayName;
+
+    DateFilters(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return this.displayName;
+    }
+}
--- src/com/ga/acme/temp.java
+++ src/com/ga/acme/temp.java
@@ -2,8 +2,7 @@ package com.ga.acme;
 
 
 import com.ga.acme.controllers.Transaction;
-import com.ga.acme.enums.AccountType;
-import com.ga.acme.enums.CardType;
+import com.ga.acme.enums.DateFilters;
 
 public class temp {
 
@@ -14,17 +13,20 @@ public class temp {
 //        Account.addAccount("1", AccountType.SAVINGSACCOUNT, true, false, false);
 //        Account.addAccount("2", AccountType.CHECKINGACCOUNT, true, false, false);
 //        Account.addAccount("2", AccountType.SAVINGSACCOUNT, true, false, false);
-
+//
 //        Auth.login("1", "potato");
-
+//
 //        Transaction.withdraw("1", 100, AccountType.SAVINGSACCOUNT, CardType.MASTERCARD);
 //        Transaction.resolveOverdraft("1", 271, AccountType.SAVINGSACCOUNT);
-        Transaction.deposit("1", 200, AccountType.SAVINGSACCOUNT, CardType.MASTERCARD);
+//        Transaction.deposit("1", 200, AccountType.SAVINGSACCOUNT, CardType.MASTERCARD);
 //        Transaction.transfer("1", 10, AccountType.SAVINGSACCOUNT, CardType.MASTERCARD, "2", AccountType.CHECKINGACCOUNT, true);
-
+//
 //        Transaction.deposit("1", 100, AccountType.SAVINGSACCOUNT, CardType.MASTERCARD);
+//
+//        Transaction.printUserAccountStatement("1", AccountType.CHECKINGACCOUNT);
 
-        Transaction.getUserTransactions("1");
+        Transaction.printFilteredTransactions("1", DateFilters.LAST_WEEK);
+
 
     }
 }
