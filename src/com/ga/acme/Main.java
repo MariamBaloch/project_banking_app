6e3188a3126abb0b1ea93afbd1906e645e0e94f7
MariamBaloch <maryamhoth@gmail.com>
2026-09-14 17:29:05 +0300

    rename main file

--- src/com/ga/acme/Main.java
+++ src/com/ga/acme/Main.java
@@ -12,7 +12,7 @@ import com.ga.acme.services.AuthService;
 
 import java.util.Scanner;
 
-public class temp {
+public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
